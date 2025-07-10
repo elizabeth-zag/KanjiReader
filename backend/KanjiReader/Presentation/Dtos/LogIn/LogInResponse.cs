7c5a75b5a7df834e680a84bf98ddbb807be4694f
Elizaveta <elizabeth_zag@mail.ru>
2025-07-10 08:28:48 +0700

    authentication authorization

---
+++ backend/KanjiReader/Presentation/Dtos/LogIn/LogInResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace KanjiReader.Presentation.Dtos.LogIn;
+
+public class LogInResponse
+{
+    public ClaimsIdentity? ClaimsIdentity { get; set; }
+    public string ErrorMessage { get; set; } = "";
+}
