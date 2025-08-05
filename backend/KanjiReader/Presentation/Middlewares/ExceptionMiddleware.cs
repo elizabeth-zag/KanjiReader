2b5075902aca8c9c87e07ef4b4683ab4e4c9f68c
Elizaveta <elizabeth_zag@mail.ru>
2025-08-05 19:48:22 +0700

    exception handling

---
+++ backend/KanjiReader/Presentation/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace KanjiReader.Presentation.Middlewares;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception");
+            
+            var statusCode = ex switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,
+                InvalidOperationException => StatusCodes.Status400BadRequest,
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+                _ => StatusCodes.Status500InternalServerError
+            };
+            
+            context.Response.StatusCode = statusCode;
+            context.Response.ContentType = "application/json";
+            var error = new { Error = ex.Message };
+            await context.Response.WriteAsJsonAsync(error);
+        }
+    }
+}
