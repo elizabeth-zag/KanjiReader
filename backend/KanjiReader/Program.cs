f809fd86913a482b8d74646aaa9c7d9633de9b05
Elizaveta <elizabeth_zag@mail.ru>
2025-08-05 19:48:22 +0700

    exception handle

--- backend/KanjiReader/Program.cs
+++ backend/KanjiReader/Program.cs
@@ -1,10 +1,22 @@
-﻿using KanjiReader.Domain.EventHandlers;
-using KanjiReader.Domain.EventHandlers.StartGenerating;
-using KanjiReader.Domain.EventHandlers.WatanocParsing;
+﻿using System.Text.Json.Serialization;
+using Hangfire;
+using Hangfire.PostgreSql;
+using KanjiReader.Domain.Deletion;
+using KanjiReader.Domain.DomainObjects.TextProcessingData;
+using KanjiReader.Domain.DomainObjects.TextProcessingData.BaseData;
+using KanjiReader.Domain.GenerationRules;
+using KanjiReader.Domain.Jobs;
 using KanjiReader.Domain.Kanji;
 using KanjiReader.Domain.Kanji.WaniKani;
-using KanjiReader.Domain.Text;
+using KanjiReader.Domain.TextProcessing;
+using KanjiReader.Domain.TextProcessing.Handlers;
+using KanjiReader.Domain.TextProcessing.Handlers.NhkParsing;
+using KanjiReader.Domain.TextProcessing.Handlers.SatoriParsing;
+using KanjiReader.Domain.TextProcessing.Handlers.WatanocParsing;
 using KanjiReader.Domain.UserAccount;
+using KanjiReader.ExternalServices.JapaneseTextSources.GoogleGenerativeAI;
+using KanjiReader.ExternalServices.JapaneseTextSources.Nhk;
+using KanjiReader.ExternalServices.JapaneseTextSources.SatoriReader;
 using KanjiReader.ExternalServices.JapaneseTextSources.Watanoc;
 using KanjiReader.ExternalServices.KanjiApi;
 using KanjiReader.ExternalServices.WaniKani;
@@ -13,7 +25,7 @@ using KanjiReader.Infrastructure.Database.Models;
 using KanjiReader.Infrastructure.Database.Repositories;
 using KanjiReader.Infrastructure.Redis;
 using KanjiReader.Infrastructure.Repositories;
-using KanjiReader.Mapper;
+using KanjiReader.Presentation.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -23,7 +35,6 @@ using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
-    
 
 builder.Services.AddDbContext<KanjiReaderDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("SqlConnection")));
@@ -32,7 +43,7 @@ builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<KanjiReaderDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
         ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("REDIS_URL")!));
 
 builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
@@ -43,7 +54,6 @@ builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
         options.Cookie.SameSite = SameSiteMode.None;
         options.LoginPath = "/api/auth/login";
         options.LogoutPath = "/api/auth/logout";
-        options.ExpireTimeSpan = TimeSpan.FromHours(1); // Optional: cookie lifetime
     });
 
 // todo: for dev purposes
@@ -56,40 +66,55 @@ builder.Services.AddCors(options =>
             .AllowCredentials());
 });
 
-builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
-builder.Services.AddControllers();
 builder.Services.AddHttpClient(); 
 
 builder.Services.AddScoped<WaniKaniClient>();
-builder.Services.AddScoped<WatanocClient>();
 builder.Services.AddScoped<KanjiApiClient>();
+
+builder.Services.AddScoped<WatanocClient>();
 builder.Services.AddScoped<NhkClient>();
 builder.Services.AddScoped<SatoriReaderClient>();
+
 builder.Services.AddScoped<GoogleGenerativeAiClient>();
+
+builder.Services.AddScoped<WatanocParsingHandler>();
+builder.Services.AddScoped<NhkParsingHandler>();
+builder.Services.AddScoped<SatoriParsingHandler>();
 
 builder.Services.AddScoped<WaniKaniService>();
 builder.Services.AddScoped<UserAccountService>();
 builder.Services.AddScoped<KanjiService>();
-builder.Services.AddScoped<CreateEventsService>();
 builder.Services.AddScoped<TextService>();
+builder.Services.AddScoped<TextParsingService>();
+builder.Services.AddScoped<DeletionService>();
+builder.Services.AddScoped<TextProcessingHandlersFactory>();
+builder.Services.AddScoped<IGenerationRulesService<NhkParsingData, NhkParsingBaseData>, NhkRulesService>();
+builder.Services.AddScoped<IGenerationRulesService<WatanocParsingData, WatanocParsingBaseData>, WatanocRulesService>();
+builder.Services.AddScoped<IGenerationRulesService<SatoriParsingData, SatoriParsingBaseData>, SatoriRulesService>();
 
-builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IKanjiCacheRepository, RedisKanjiCacheRepository>();
 builder.Services.AddScoped<IKanjiRepository, KanjiRepository>();
 builder.Services.AddScoped<IProcessingResultRepository, ProcessingResultRepository>();
 builder.Services.AddScoped<IUserGenerationStateRepository, UserGenerationStateRepository>();
 
 builder.Services.AddHangfire(config =>
-    config.UsePostgreSqlStorage(c =>
-        c.UseNpgsqlConnection(builder.Configuration.GetConnectionString("SqlConnection"))));
+    config.UsePostgreSqlStorage(
+        c => c.UseNpgsqlConnection(builder.Configuration.GetConnectionString("SqlConnection"))));
 
-builder.Services.AddHangfireServer();
+builder.Services.AddHangfireServer(options =>
+{
+    options.ServerTimeout = TimeSpan.FromMinutes(1);
+});
 
 var app = builder.Build();
 
 app.UseHangfireDashboard();
-BackgroundJob.Enqueue<EventHandlingJob>(svc => svc.Execute());
 
 app.UseHttpsRedirection();
 app.UseCors("MyPolicy");
@@ -99,15 +124,30 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseMiddleware<ExceptionMiddleware>();
+
+
+RecurringJob.AddOrUpdate<DeleteUnusedDataJob>(
+    nameof(DeleteUnusedDataJob),
+    job => job.Execute(CancellationToken.None),
+    Cron.Daily);
+
+// retry stuck in processing jobs
+var api = JobStorage.Current.GetMonitoringApi();
+var processingJobs = api.ProcessingJobs(0, 100);
+var servers = api.Servers();
+var orphanJobs = processingJobs.Where(j => servers.All(s => s.Name != j.Value.ServerId));
+foreach (var orphanJob in orphanJobs)
+{
+    BackgroundJob.Requeue(orphanJob.Key);
+}
+
 app.Run();
 
-
-
-// AI generated texts
-// More text sources
-// exception middleware
-// retries
-// filter existing texts on repeated searches
-// maybe use hangfire/quartz for background tasks
-// remove events after processing
-// start generation event
+// ask user to remove previous texts before generating new ones
+// logging
+// add email notification
+// maybe add wanikani and threshold customization 
+// add indices
+// add warning for insufficient kanji count
+// cache for website calls
