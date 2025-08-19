8a293eb4ec0cedec16f171cce9cf186e89a717c6
Elizaveta <elizabeth_zag@mail.ru>
2025-08-19 23:26:36 +0700

    email sender

--- backend/KanjiReader/Presentation/Dtos/Login/UpdateUserInfo/UpdateEmailRequest.cs
+++ backend/KanjiReader/Presentation/Dtos/Login/UpdateUserInfo/UpdateEmailRequest.cs
@@ -5,6 +5,6 @@ namespace KanjiReader.Presentation.Dtos.Login.UpdateUserInfo;
 public class UpdateEmailRequest
 {
     [EmailAddress]
-    public string Email { get; set; }
+    public string? Email { get; set; }
     public bool NeedDelete { get; set; }
 }
